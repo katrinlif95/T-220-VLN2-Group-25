2cdcfcf833d96b04e69df9d8ffb9917b0bce80ac
katrinlif95 <katrinlif95@gmail.com>
2026-05-05 12:37:24 +0000

    Add homepage and initial home template + a few small changes

--- artvault/settings.py
+++ artvault/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'artwork',
+    'seller',
+    'pages',
+    'user',
 ]
 
 MIDDLEWARE = [
