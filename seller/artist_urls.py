35306ded0e4f6e6c5c094b7695d59a5a0998f47c
erna-osk <ernaosk1@gmail.com>
2026-05-06 10:27:42 +0000

    laga urls í fleirtölu

--- seller/artist_urls.py
+++ seller/artist_urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # http://127.0.0.1:8000/artist
+    # http://127.0.0.1:8000/artists/
     path('', views.artist_index, name='artist-index'),
 
-    # http://127.0.0.1:8000/artist/{id}
-    path('<int:id>', views.get_artist_by_id, name='artist-by-id'),
+    # http://127.0.0.1:8000/artists/{id}/
+    path('<int:id>/', views.get_artist_by_id, name='artist-by-id'),
 ]
--- seller/gallery_urls.py
+++ seller/gallery_urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # http://127.0.0.1:8000/gallery
+    # http://127.0.0.1:8000/galleries/
     path('', views.gallery_index, name='gallery-index'),
 
-    # http://127.0.0.1:8000/gallery/{id}
-    path('<int:id>', views.get_gallery_by_id, name='gallery-by-id'),
+    # http://127.0.0.1:8000/galleries/{id}/
+    path('<int:id>/', views.get_gallery_by_id, name='gallery-by-id'),
 ]
