88603922f089620fb6971df9e9eb4c68c55576a1
erna-osk <ernaosk1@gmail.com>
2026-05-07 09:58:02 +0000

    birta user bids fra db a account profile

--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from bid.models import Bid
+from django.contrib.auth.models import User  #má taka út þegar login flow er orðið virkt og hardcoded test búið
+
 # Main account/profile page
 # Displays general user profile information
 def profile_detail(request):
@@ -13,10 +16,29 @@ def profile_detail(request):
 
 # Page showing all bids belonging to the logged-in user
 def account_bids(request):
+    # FINAL VERSION : Get all bids belonging to current logged-in user
+    # bids = Bid.objects.filter(
+        #user=request.user
+    #)
+
+
+    # TEMPORARY TEST VERSION: (svo það þurfi ekki að vera logged in til að sjá bids)
+    # Manually fetch a specific test user from database
+    test_user = User.objects.get(
+        username="User1"
+    )
+
+    # Get bids belonging to test user
+    bids = Bid.objects.filter(
+        user=test_user
+    )
 
     return render(
         request,
-        "user/account_bids.html"
+        "user/account_bids.html",
+        {
+            "bids": bids
+        }
     )
 
 
